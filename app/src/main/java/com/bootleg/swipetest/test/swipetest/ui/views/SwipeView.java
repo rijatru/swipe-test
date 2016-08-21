a6a62d3fe05c4a60059e2eed1f77cb93921b7173
rijatru <rijatru@gmail.com>
2016-08-21 01:48:29 -0500

    Swipe animation + alpha animation

--- app/src/main/java/com/bootleg/swipetest/test/swipetest/ui/views/SwipeView.java
+++ app/src/main/java/com/bootleg/swipetest/test/swipetest/ui/views/SwipeView.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.OvershootInterpolator;
 import android.widget.FrameLayout;
 
@@ -21,6 +22,11 @@ public class SwipeView extends FrameLayout {
     private float dX;
     private float x1;
     private boolean notAnimating = true;
+
+    AnimatorListenerAdapter moveListener;
+
+    private int DIRECION_LEFT = 0;
+    private int DIRECION_RIGHT = 1;
 
     public SwipeView(Context context) {
         super(context);
@@ -51,7 +57,26 @@ public class SwipeView extends FrameLayout {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         binding = ViewSwipeLayoutBinding.inflate(inflater, this, true);
 
+        initMoveListener();
         initSwipeListener();
+    }
+
+    private void initMoveListener() {
+
+        moveListener = new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+                notAnimating = false;
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+
+                notAnimating = true;
+            }
+        };
     }
 
     private void initSwipeListener() {
@@ -75,40 +100,81 @@ public class SwipeView extends FrameLayout {
 
                     case MotionEvent.ACTION_MOVE:
 
-                        view.animate()
-                                .x(event.getRawX() + dX)
-                                .setDuration(0)
-                                .start();
+                        view.setX(event.getRawX() + dX);
+
+                        setViewAlpha(view);
+
                         break;
+
                     case MotionEvent.ACTION_UP:
 
-                        long eventDuration = SystemClock.elapsedRealtime() - event.getDownTime();
+                        setActionUp(view, event);
 
-                        Log.d("Test", "velocity: " + (Math.abs(x1 - Math.abs(view.getX())) / eventDuration) + " p/ms " + "eventDuration: " + eventDuration);
-
-                        view.animate()
-                                .x(x1)
-                                .setDuration(400)
-                                .setInterpolator(new OvershootInterpolator())
-                                .setListener(new AnimatorListenerAdapter() {
-                                    @Override
-                                    public void onAnimationStart(Animator animation) {
-
-                                        notAnimating = false;
-                                    }
-
-                                    @Override
-                                    public void onAnimationEnd(Animator animation) {
-                                        notAnimating = true;
-                                    }
-                                })
-                                .start();
                         break;
+
                     default:
+
                         return false;
                 }
+
                 return true;
             }
         });
+    }
+
+    private void setViewAlpha(View view) {
+
+        float outsideSection;
+        float alpha = 1;
+
+        if (view.getX() + view.getWidth() > getRootView().getRight()) {
+
+            outsideSection = view.getX() + view.getWidth() - getRootView().getRight();
+
+            alpha = 1 - outsideSection / view.getWidth();
+
+            Log.d("Test RIGHT", "outsideSection: " + outsideSection + " " + alpha);
+
+        } else if (view.getX() < getRootView().getX()) {
+
+            outsideSection = Math.abs(view.getX());
+
+            alpha = 1 - outsideSection / view.getWidth();
+
+            Log.d("Test LEFT", "outsideSection: " + outsideSection + " " + alpha);
+        }
+
+        view.setAlpha(alpha);
+    }
+
+    private void setActionUp(View view, MotionEvent event) {
+
+        float time = SystemClock.elapsedRealtime() - event.getDownTime();
+
+        float distance = view.getX() < 0 ? Math.abs(view.getX()) + x1 : view.getX() - x1;
+
+        float velocity = distance / time;
+
+        int animDuration = (int) ((view.getX() > 0 ? getRootView().getRight() - view.getX() : view.getRight()) / velocity);
+
+        if (view.getX() > getRootView().getRight() / 2 || view.getX() + view.getWidth() < getRootView().getRight() / 2) {
+
+            view.animate()
+                    .x((view.getX() < 0 ? -1 : 1) * getRootView().getRight())
+                    .alpha(0)
+                    .setDuration(animDuration > 300 ? 300 : animDuration)
+                    .setListener(moveListener)
+                    .start();
+
+        } else {
+
+            view.animate()
+                    .x(x1)
+                    .alpha(1)
+                    .setDuration(400)
+                    .setInterpolator(new OvershootInterpolator())
+                    .setListener(moveListener)
+                    .start();
+        }
     }
 }
