2cef08380cadbb54064e5f08654c599b07f7159c
rijatru <rijatru@gmail.com>
2016-08-26 21:26:31 -0500

    test

--- app/src/main/java/com/bootleg/swipetest/test/swipetest/MainActivity.java
+++ app/src/main/java/com/bootleg/swipetest/test/swipetest/MainActivity.java
@@ -1,18 +1,27 @@
 package com.bootleg.swipetest.test.swipetest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.bootleg.swipetest.test.swipetest.ui.views.activities.BaseFragmentActivity;
+import com.bootleg.swipetest.test.swipetest.ui.views.fragments.SwipeViewFragment;
+
+public class MainActivity extends BaseFragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_base);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        addFragment(new SwipeViewFragment());
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
