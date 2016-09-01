5de9dcc8df30b01dd6faf80ce4ce12b8cae6f21d
rijatru <rijatru@gmail.com>
2016-09-01 11:38:35 -0500

    refactor

--- app/src/main/java/com/bootleg/swipetest/test/swipetest/factories/AdapterFactory.java
+++
@@ -1,9 +0,0 @@
-package com.bootleg.swipetest.test.swipetest.factories;
-
-import com.bootleg.swipetest.test.swipetest.interfaces.GenericObject;
-import com.bootleg.swipetest.test.swipetest.interfaces.ItemView;
-
-public abstract class AdapterFactory {
-
-    public abstract ItemView onCreateView(GenericObject item);
-}
